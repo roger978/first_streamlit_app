de1c992af16baba98370678b69d7ffb17f5ae607
roger978 <droger978@gmail.com>
2023-08-03 10:31:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('first project') 
-streamlit.header('List of options')
-streamlit.text('Texting again')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg 🥣 🥗 🐔 🥑🍞')
